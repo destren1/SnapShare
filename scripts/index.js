7246e99ee012a8795c7acd47605d57565181a6f2
Ivan Romanovskiy <ivanromanovskiy0412@gmail.com>
2023-11-18 18:09:40 +0300

    add:cards,delete button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+const addCard = (cardData, deleteCallBack) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
-// @todo: Функция создания карточки
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCallBack(cardElement);
+  });
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+};
+
+initialCards.forEach((item) => {
+  const newCard = addCard(item, (cardElement) => {
+    cardElement.remove();
+  });
+  placesList.append(newCard);
+});
