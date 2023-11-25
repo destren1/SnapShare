eaf6f804a9738aac1e07649317411d2f92b61814
Ivan Romanovskiy <ivanromanovskiy0412@gmail.com>
2023-11-25 18:09:21 +0300

    setting: Webpack uses Babel in the build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	entry: { main: './src/index.js' },
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'main.js',
-						publicPath: ''
-},
-mode: 'development',
-devServer: {
-	static: path.resolve(__dirname, './dist'), 
-	compress: true, 
-	port: 8080, 
-	open: true 
-},
-}
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+	module: {
+    rules: [ 
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      }
+      ]
+  }
+};
