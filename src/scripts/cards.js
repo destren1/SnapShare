6defa493afd207c8254b3f4348a5f4907e4353c8
Ivan Romanovskiy <ivanromanovskiy0412@gmail.com>
2023-11-27 13:50:47 +0300

    add: links to images

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,26 +1,51 @@
+const Arkhyz = new URL(
+  "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  import.meta.url
+);
+const Chelyabinsk = new URL(
+  "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  import.meta.url
+);
+const Ivanovo = new URL(
+  "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  import.meta.url
+);
+const Kamchatka = new URL(
+  "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  import.meta.url
+);
+const KholmogorskyRayon = new URL(
+  "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  import.meta.url
+);
+const Baikal = new URL(
+  "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  import.meta.url
+);
+
 const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: Arkhyz,
+  },
+  {
+    name: "Челябинская область",
+    link: Chelyabinsk,
+  },
+  {
+    name: "Иваново",
+    link: Ivanovo,
+  },
+  {
+    name: "Камчатка",
+    link: Kamchatka,
+  },
+  {
+    name: "Холмогорский район",
+    link: KholmogorskyRayon,
+  },
+  {
+    name: "Байкал",
+    link: Baikal,
+  },
 ];
