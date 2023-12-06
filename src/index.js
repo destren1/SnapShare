16901f13e33391244971fe0dfcaf908d604ab46b
Ivan Romanovskiy <ivanromanovskiy0412@gmail.com>
2023-12-07 00:53:51 +0300

    create: function popupInteractions with new functions openPopup&& closePopup

--- src/index.js
+++ src/index.js
@@ -32,35 +32,59 @@ initialCards.forEach((item) => {
 
 const page = document.querySelector(".page");
 const editButton = page.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
 const addButton = page.querySelector(".profile__add-button");
-const ButtonClosePopup = document.querySelector(".popup__close");
 
-const openPopup = () => {
-  popup.classList.add("popup_is-opened");
+const popupNewCard = document.querySelector(".popup_type_new-card");
+const popupEdit = document.querySelector(".popup_type_edit");
+
+const openPopupNewCard = () => {
+  popupNewCard.classList.add("popup_is-opened");
   document.addEventListener("keydown", closeOnEsc);
+};
+const openPopupEdit = () => {
+  popupEdit.classList.add("popup_is-opened");
+  document.addEventListener("keydown", closeOnEsc);
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
 };
 
 const closePopup = () => {
-  popup.classList.remove("popup_is-opened");
+  popupEdit.classList.remove("popup_is-opened");
+  popupNewCard.classList.remove("popup_is-opened");
   document.removeEventListener("keydown", closeOnEsc);
 };
-
 const closeOnEsc = (evt) => {
   if (evt.key === "Escape") {
     closePopup();
   }
-}; 
+};
 
-page.addEventListener("click", (evt) => {
-  if (evt.target === editButton || evt.target === addButton) {
-    openPopup();
-  } else {
-    const popupContent = evt.target.closest(".popup__content");
-    if (!popupContent || evt.target === ButtonClosePopup) {
-      closePopup();
-    }
+const popupInteraction = (evt) => {
+  if (evt.target === editButton) {
+    openPopupEdit();
+  } else if (evt.target === addButton) {
+    openPopupNewCard();
+  } else if (evt.target.classList.contains("popup__close")) {
+    closePopup();
+  } else if (!evt.target.closest("popup__content")) {
+    closePopup();
   }
-});
+};
 
+page.addEventListener("click", popupInteraction);
 
+const formElement = document.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__input_type_name");
+const jobInput = formElement.querySelector(".popup__input_type_description");
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
